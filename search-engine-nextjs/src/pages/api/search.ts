bce9799e230181a612f5f2cc8be85292c445b55c
NutraloxidE <niconico.yokato@gmail.com>
2024-07-03 16:26:49 +0900

    stable - added the or search
    put space between words for "OR" search

--- search-engine-nextjs/src/pages/api/search.ts
+++ search-engine-nextjs/src/pages/api/search.ts
@@ -10,16 +10,26 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 
     console.log("Search term:", req.query.searchterm);
 
+    // Split the search term by space for OR search
+    const orTerms = (searchterm as string).split(' ');
+
+    console.log("OR terms:", orTerms); // Output OR terms
+
+    // Create the query
+    const query = {
+        $or: orTerms.map(term => ({
+            $or: [
+                { title: { $regex: new RegExp(term, 'i') } },
+                { about: { $regex: new RegExp(term, 'i') } },
+                { textSnippet: { $regex: new RegExp(term, 'i') } },
+                { url: { $regex: new RegExp(term, 'i') } }
+            ]
+        }))
+    };
+
     // Search the database for the search term
-    const results = await Data.find({
-        $or: [
-            { title: { $regex: new RegExp(searchterm as string, 'i') } },
-            { about: { $regex: new RegExp(searchterm as string, 'i') } },
-            { textSnippet: { $regex: new RegExp(searchterm as string, 'i') } },
-            { url: { $regex: new RegExp(searchterm as string, 'i') } } // 追加した行
-        ]
-    });
-    
+    const results = await Data.find(query);
+
     // Return the results
     res.status(200).json(results);
 }
