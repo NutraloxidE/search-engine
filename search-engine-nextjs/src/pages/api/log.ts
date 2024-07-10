9c7d5d11e53747f7fd3c9ed76dd9db76d9ad5fa8
NutraloxidE <niconico.yokato@gmail.com>
2024-07-10 09:12:43 +0900

    wip - now logs records time!
    a

--- search-engine-nextjs/src/pages/api/log.ts
+++ search-engine-nextjs/src/pages/api/log.ts
@@ -7,7 +7,9 @@ import path from 'path';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { ip, url } = req.body;
 
-  const log = `IP: ${ip}, URL: ${url}\n`;
+  //time with 6 digits of precision in JST
+  const time = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+  const log = "["+time+"]"+ `IP: ${ip}, URL: ${url}\n`;
   
   const logFilePath = path.join(process.cwd(), 'logs', 'access.log');
   fs.appendFileSync(logFilePath, log);
