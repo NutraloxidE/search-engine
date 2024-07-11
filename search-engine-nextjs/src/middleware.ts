c1255a9378516e2efbae12559e05f117a1fa1486
NutraloxidE <niconico.yokato@gmail.com>
2024-07-11 20:23:42 +0900

    wip - it kills hacker now

--- search-engine-nextjs/src/middleware.ts
+++ search-engine-nextjs/src/middleware.ts
@@ -25,7 +25,40 @@ export async function middleware(request: NextRequest) {
     body: JSON.stringify(logData),
   });
 
+  // hackerKiller関数を実行し、その結果に応じてレスポンスを返す
+  const hackerKillerResponse = hackerKiller(request);
+  if (hackerKillerResponse) {
+    return hackerKillerResponse;
+  }
+
   return NextResponse.next();
+}
+
+function hackerKiller(request: NextRequest) {
+  // リクエストのURLを取得
+  const { pathname } = new URL(request.url);
+
+  // Restricted path list
+  const restrictedPaths = [
+    '/admin',
+    '/wp-admin',
+    '/wp-login',
+    '/login',
+    '/admin.php',
+    '/wp-admin.php',
+    '/wp-login.php',
+    '/login.php',
+    '/cgi-bin',
+    '/boaform'
+  ];
+
+  // if the request path is in the restricted path list, return a custom response
+  if (restrictedPaths.some(path => pathname.includes(path))) {
+    return new Response('Fuck off', { status: 420 });
+  }
+
+  // restricted pathに含まれない場合は、nullを返す
+  return null;
 }
 
 export const config = {
