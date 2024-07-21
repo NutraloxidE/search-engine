18c10200e3ff181e6184bf56aaa48e1d332f6960
NutraloxidE <niconico.yokato@gmail.com>
2024-07-22 02:42:36 +0900

    wip - visual being fixed result ul is now same size each

--- search-engine-nextjs/src/app/register/page.tsx
+++ search-engine-nextjs/src/app/register/page.tsx
@@ -1,4 +1,4 @@
-// src/pages/login.tsx
+// src/app/register/page.tsx
 'use client';
 
 import React from 'react';
@@ -7,8 +7,8 @@ import Register from '../../components/Register';
 const SignInPage: React.FC = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-transparent" style={{transform: 'translateY(-20vh)'}}>
-        <div className="w-full max-w-lg ml-5 mr-5 mt-2 pl-2 pr-2">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-transparent" style={{transform: 'translateY(-10vh)'}}>
+        <div className="w-full max-w-lg ml-5 mr-5 mt-15 pl-2 pr-2">
           <div className="p-4 pt-4 pb-4 flex items-center py-2 shadow-neumorphism rounded-md bg-gray-200">
             <Register />
           </div>
